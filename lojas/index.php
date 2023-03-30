7031d317ed0e44ece54c44ad395d70edd6d602cd
Helio Tradesis <helio@tradesis.com.br>
2023-03-30 12:10:57 -0300

    tslebes v0

--- lojas/index.php
+++ lojas/index.php
@@ -42,32 +42,7 @@ include_once 'conexao.php';
                             <span class="fs-5 text">Clientes</span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                            <a class="dropdown-item" href="#" src="clientes/seguros_parametros.php">Seguros</a>
-                            <a class="dropdown-item" href="#" src="clientes/historico_parametros.php">Histórico Clientes</a>
-                            <a class="dropdown-item" href="#" src="clientes/contratos_parametros.php">Contratos</a>
                             <a class="dropdown-item" href="#" src="clientes/cupomcashback_parametros.php">Cupom Cashback</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="fs-5 text">Consultas</span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                            <a class="dropdown-item" href="#" src="consultas/relatorios.php">Relatórios</a>
-                            <a class="dropdown-item" href="#" src="consultas/desconto_parametros.php">Descontos</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="fs-5 text">Relatorios</span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                            <a class="dropdown-item" href="#" src="relatorios/frsalcart.php">Vencidos e a vencer</a>
-                            <a class="dropdown-item" href="#" src="relatorios/relqtdNovo.php">Liquidações diarias p/ periodo</a>
-                            <a class="dropdown-item" href="#" src="relatorios/cred01.php">Extrato de cobrança simples</a>
-                            <a class="dropdown-item" href="#" src="relatorios/cre01_ma.php">Posição de cliente por periodo - A</a>
-                            <a class="dropdown-item" href="#" src="relatorios/cre01_lp.php">Posição de cliente por periodo - B</a>
-                            
                         </div>
                     </li>
 
