ee9be592eb10de3628d3ea11f66dd63e1fe1f51d
Gabriel Bettencourt <gabriel.vieira@tradesis.com.br>
2023-04-10 11:17:58 -0300

    Revert "modal"
    This reverts commit f009041810d296b23aaef50df0de494ac46d659c.

--- lojas/relatorios/relqtdNovo.php
+++ lojas/relatorios/relqtdNovo.php
@@ -1,4 +1,3 @@
-
 <?php
 // gabriel 09022023 15:35
 
@@ -20,20 +19,17 @@ $relatorios = buscaRelatorios($progcod,$parametros);
 <!DOCTYPE html>
 <html lang="pt-BR">
 
+
+
+
 <body class="bg-transparent">
 
     <div class="container-fluid mt-3">
         <div class="card">
             <div class="card-header">
                 <div class="row">
-                    <div class="col-9">
-                        <h3 class="col">Liquidações diarias p/ periodo</h3>
-                    </div>
-                    <div class="col-1" style="text-align:right">
-                        <a href="#" role="button" class="btn btn-info btn-sm" onClick="window.location.reload()">
-                            Atualizar
-                            Relatórios
-                        </a>
+                    <div class="col-10">
+                        <h3 class="col">Resumo liquidações diarias p/ periodo</h3>
                     </div>
                     <div class="col-2" style="text-align:right">
                         <a href="relqtdNovo_inserir.php" role="button" class="btn btn-success btn-sm">Novo Relatório</a>
@@ -43,16 +39,17 @@ $relatorios = buscaRelatorios($progcod,$parametros);
         </div>
         <div class="table table-sm table-hover">
             <table class="table">
+                <!--<table id="tabela" class="table table-bordered mb-0">-->
+
                 <thead class="thead-light">
                     <tr>
-                        <th class="text-center">ID</th>
                         <th class="text-center">Usuário</th>
                         <th class="text-center">Data</th>
                         <th class="text-center">Hora</th>
+                        <th class="text-center">Programa</th>
                         <th class="text-center">Nome do relatório</th>
                         <th class="text-center">Nome do arquivo</th>
                         <th class="text-center">REMOTE_ADDR</th>
-                        <th class="text-center">Parâmetros</th>
                         <th class="text-center">PDF</th>
                     </tr>
                 </thead>
@@ -60,16 +57,13 @@ $relatorios = buscaRelatorios($progcod,$parametros);
                 foreach ($relatorios as $relatorio) {
                 ?>
                     <tr>
-                        <td class="text-center"><?php echo $relatorio['idRelat'] ?></td>
                         <td class="text-center"><?php echo $relatorio['usercod'] ?></td>
                         <td class="text-center"><?php echo $relatorio['dtinclu'] ?></td>
                         <td class="text-center"><?php echo $relatorio['hrinclu'] ?></td>
                         <td class="text-center"><?php echo $relatorio['progcod'] ?></td>
+                        <td class="text-center"><?php echo $relatorio['relatnom'] ?></td>
                         <td class="text-center"><?php echo $relatorio['nomeArquivo'] ?></td>
                         <td class="text-center"><?php echo $relatorio['REMOTE_ADDR'] ?></td>
-                        <td class="text-center">
-                            <a class="btn btn-sm" href="#parametros?idRelat=<?php echo $relatorio['idRelat'] ?>" data-toggle="modal">Parâmetros</a>
-                        </td>
                         <td class="text-center">
                             <a class="btn btn-sm" href="visualizar.php?nomeArquivo=<?php echo $relatorio['nomeArquivo'] ?>">Visualizar</a>
                         </td>
@@ -79,34 +73,7 @@ $relatorios = buscaRelatorios($progcod,$parametros);
         </div>
     </div>
 
-    <div class="modal fade" id="parametros" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="ModalLabel">Parâmetros do Relatório</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <?php
-                if (isset($_GET['idRelat'])) {
-                    $relatparam = buscaParametros($_GET['idRelat'])['parametros'];
-
-                ?>
-                <div class="modal-body">
-                    <div class="col-4">
-                        <label>Filial</label>
-                        <input type="text" class="form-control" value="<?php echo $relatparam['codigoFilial'] ?>" readonly>
-                        <label>Data Inicial</label>
-                        <input type="text" class="form-control" value="<?php echo $relatparam['dataInicial'] ?>" readonly>
-                        <label>Data Final</label>
-                        <input type="text" class="form-control" value="<?php echo $relatparam['dataFinal'] ?>" readonly>
-                    </div>
-                </div>
-                <?php } ?>
-            </div>
-        </div>
-    </div>
+   
 
 </body>
 
