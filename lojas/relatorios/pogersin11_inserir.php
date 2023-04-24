e7be587fb93e4a4267d2fe6a72aa16159d131174
Gabriel Bettencourt <gabriel.vieira@tradesis.com.br>
2023-04-24 13:09:20 -0300

    lojas/relatorios

--- lojas/relatorios/pogersin11_inserir.php
+++ lojas/relatorios/pogersin11_inserir.php
@@ -26,43 +26,77 @@ $filial = $filial[2];
                         <div class="col">
                             <label>Usuário</label>
                             <div class="form-group">
-                                <input type="text" name="usercod" class="form-control" value="Lebes" autocomplete="off" readonly>
+                                <input type="text" name="usercod" class="form-control" value="Lebes" autocomplete="off"
+                                    readonly>
                             </div>
                         </div>
                         <div class="col">
                             <label>Programa</label>
                             <div class="form-group">
-                                <input type="text" name="progcod" class="form-control" value="pogersin11" autocomplete="off" readonly>
+                                <input type="text" name="progcod" class="form-control" value="pogersin11"
+                                    autocomplete="off" readonly>
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="form-group col">
                             <label>Nome do relatório</label>
-                            <input type="text" name="relatnom" class="form-control" value="Posicao financeira vencidos/a vencer" autocomplete="off" readonly>
+                            <input type="text" name="relatnom" class="form-control"
+                                value="Posicao financeira vencidos/a vencer" autocomplete="off" readonly>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col">
+                        <div class="col-4">
                             <div class="form-group">
                                 <label>Cliente</label>
                                 <select class="form-control" name="cliente">
-                                    <option value="yes">Geral</option>
-                                    <option value="no">Facil</option>
+                                    <option value="Sim">Geral</option>
+                                    <option value="Nao">Facil</option>
                                 </select>
                             </div>
                             <div class="form-group">
-                                <label>Considerar apenas feirao</label>
-                                <select class="form-control" name="considerafeirao">
+                                <label>Modalidade</label>
+                                <select class="form-control" name="modalidade[]" multiple
+                                    style="height: 120px; overflow-y: hidden;">
+                                    <option value="CRE">CRE</option>
+                                    <option value="CP">CP</option>
+                                    <option value="CP0">CP0</option>
+                                    <option value="CP1">CP1</option>
+                                    <option value="CP2">CP2</option>
+                                </select>
+
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <label>Estabelecimento</label>
+                                <input type="number" placeholder="Vazio = Geral" class="form-control" name="estab">
+                            </div>
+                            <div class="form-group">
+                                <label>Filial</label>
+                                <select class="form-control" name="filial">
+                                    <option value="Nao">Nao</option>
+                                    <option value="Sim">Sim</option>
+                                </select>
+
+                            </div>
+                            <div class="form-group">
+                                <label>Considera apenas LP</label>
+                                <select class="form-control" name="consideralp">
                                     <option value="Nao">Nao</option>
                                     <option value="Sim">Sim</option>
                                 </select>
                             </div>
                         </div>
-                        <div class="col"> 
+                        <div class="col">
                             <div class="form-group">
-                                <label>Considera apenas LP</label>
-                                <select class="form-control" name="consideralp">
+                                <label>Data Referência</label>
+                                <input type="date" class="form-control" name="dataRef">
+
+                            </div>
+                            <div class="form-group">
+                                <label>Considerar apenas feirao</label>
+                                <select class="form-control" name="considerafeirao">
                                     <option value="Nao">Nao</option>
                                     <option value="Sim">Sim</option>
                                 </select>
@@ -75,38 +109,9 @@ $filial = $filial[2];
                                 </select>
                             </div>
                         </div>
-                        <div class="col-4">
-                            <div class="form-group">
-                                <label>Modalidade</label>
-                                <select class="form-control" name="modalidade" multiple>
-                                    <option value="CRE">CRE</option>
-                                    <option value="CP">CP</option>
-                                    <option value="CP0">CP0</option>
-                                    <option value="CP1">CP1</option>
-                                    <option value="CP2">CP2</option>
-                                </select>
-                            </div>
-                        </div>
+
                     </div>
-                    <hr>
-                    <h6>Por Estabelecimento:</h6>
                     <div class="row">
-                        <div class="form-group col">
-                                <label>Estabelecimento</label>
-                                <input type="number" placeholder="0 = Geral" class="form-control" name="estab">
-                            </div>
-                        <div class="form-group col">
-                            <label>Data Referência</label>
-                            <input type="date" class="form-control" name="dataRef">
-                        </div>
-                    </div>
-                    <hr>
-                    <h6>Por Filial:</h6>
-                    <div class="row">
-                        <div class="form-group col">
-                            <label>Filial</label>
-                            <input type="number" class="form-control" name="CodigoFilial">
-                        </div>
                         <div class="form-group col">
                             <label>Data Inicial</label>
                             <input type="date" class="form-control" name="dataInicial">
@@ -115,7 +120,8 @@ $filial = $filial[2];
                             <label>Data Final</label>
                             <input type="date" class="form-control" name="dataFinal">
                         </div>
-                        <input type="text" class="form-control" value="Filial <?php echo $filial ?>" name="REMOTE_ADDR" hidden>
+                        <input type="text" class="form-control" value="Filial <?php echo $filial ?>" name="REMOTE_ADDR"
+                            hidden>
                     </div>
 
                     <div class="card-footer bg-transparent" style="text-align:right">
@@ -125,7 +131,6 @@ $filial = $filial[2];
             </div>
         </div>
     </div>
-
 
 </body>
 
